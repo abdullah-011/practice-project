31dc16600057e4c25e42f08f59546e943636caa4
abdullah-011 <you@example.com>
2021-12-01 23:08:48 +0300

    add all project without Responsive

--- salim.rana/project-1/js/active.js
+++ salim.rana/project-1/js/active.js
@@ -1,10 +1,11 @@
 
 $(document).ready(function(){
 
-    $('.owl-carousel').owlCarousel({
+    $('.slider-active').owlCarousel({
         loop:true,
         margin:10,
         nav:true,
+        navText: ['<i class = "fas fa-angle-left"> </i>','<i class = "fas fa-angle-right"> </i>'],
         responsive:{
             0:{
                 items:1
@@ -18,4 +19,67 @@ $(document).ready(function(){
         }
     })
 
+
+    $('.video-popup').magnificPopup({
+        type: 'image'
+        // other options
+      });
+ 
+
+})
+
+$('.project-active ').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText: ['<i class = "fas fa-angle-left"> </i>','<i class = "fas fa-angle-right"> </i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:4
+        },
+        1200:{
+            items:4
+        }
+    }
+})
+
+
+$('.blog-activ ').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText: ['<i class = "fas fa-angle-left"> </i>','<i class = "fas fa-angle-right"> </i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1200:{
+            items:1
+        }
+    }
+})
+
+
+$('.brand-actibe').owlCarousel({
+    loop:true,
+    margin:50,
+    nav:false,
+    navText: ['<i class = "fas fa-angle-left"> </i>','<i class = "fas fa-angle-right"> </i>'],
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1200:{
+            items:6
+        }
+    }
 })
